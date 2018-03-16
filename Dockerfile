95ad2b343c3676b6ce270aa85fbea0261424ce19
Dalton Fury <daltonfury42@disroot.org>
2018-03-16 15:30:41 +0530

    Fix Database in docker

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM greyltc/lamp
 MAINTAINER Dalton Fury <daltonfury42@disroot.org>
 
-ENV MYSQL_USER ${MY_SQL_USER:-testuser}
-ENV MYSQL_PASSWORD ${MY_SQL_PASSWORD:-testpass}
+ENV MY_SQL_USER ${MY_SQL_USER:-testuser}
+ENV MY_SQL_PASSWORD ${MY_SQL_PASSWORD:-testpass}
 ENV PORT ${PORT:-443} 
 
 ADD http /srv/http/
@@ -10,4 +10,4 @@ ADD http /srv/http/
 ADD SJET.sql /root
 ADD final-setup.sh /usr/sbin/final-setup
 
-CMD start-servers; setup-mysql-user; final-setup $PORT $MYSQL_USER $MYSQL_PASSWORD; sleep infinity
+CMD start-servers; setup-mysql-user; final-setup; sleep infinity
--- final-setup.sh
+++ final-setup.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 
-export PORT=${1:-443}
-export $MYSQL_USER=$2
-export $MYSQL_PASSWORD=$3
+#export PORT=${1:-443}
+#export $MYSQL_USER=$2
+#export $MYSQL_PASSWORD=$3
 
-echo PORT=$PORT, MYSQL_USER=$MYSQL_USER MYSQL_PASSWORD=$MYSQL_PASSWORD
-echo PORT=$PORT, MYSQL_USER=$MYSQL_USER MYSQL_PASSWORD=$MYSQL_PASSWORD
+function waitForMysql {
+    while [[ $(mysqladmin ping --silent) != "mysqld is alive" ]]; do
+        printf .
+        lf=$'\n'
+        sleep 1
+    done
+    printf "$lf"
+}
+
+waitForMysql
+
+echo PORT=$PORT, MY_SQL_USER=$MY_SQL_USER MY_SQL_PASSWORD=$MY_SQL_PASSWORD
 
 #sed -i "s/8080/$PORT/" /etc/apache2/sites-enabled/000-default.conf; 
 #echo Listen $PORT >> /etc/apache2/ports.conf; 
 
 mysql -u root -e "CREATE DATABASE SJET"
+mysql -u root -e "CREATE USER '$MY_SQL_USER'@'localhost' IDENTIFIED BY '$MY_SQL_PASSWORD';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$MY_SQL_USER'@'localhost' WITH GRANT OPTION;"
+mysql -u root -e "FLUSH PRIVILEGES"
 mysql -u root SJET < /root/SJET.sql
 
 #chmod 777 -R /var/www/site/app/uploads/
