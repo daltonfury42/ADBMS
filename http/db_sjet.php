581cd7ce7fe52d29ca6a5d2ed213eb28bd373c98
Archana <archana@disroot.org>
2018-03-18 22:48:22 +0530

    trying url fix in db_sjet file

--- http/db_sjet.php
+++ http/db_sjet.php
@@ -1,6 +1,6 @@
 <?php 
 
-$url = parse_url(getenv(mysql://b74ef41375c36d:ab7530c1@us-cdbr-iron-east-05.cleardb.net/heroku_69067170d65c976?reconnect=true));
+$url = parse_url(getenv("mysql://b74ef41375c36d:ab7530c1@us-cdbr-iron-east-05.cleardb.net/heroku_69067170d65c976?reconnect=true"));
 
 $servername = $url["host"];
 $username = $url["user"];
@@ -10,6 +10,7 @@ $db = substr($url["path"], 1);
 
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $db);
+//$conn = mysqli_connect($servername, $username, $password, $db);
+$conn = new mysqli($server, $username, $password, $db);
  if(!$conn)echo "hi";
 ?>
