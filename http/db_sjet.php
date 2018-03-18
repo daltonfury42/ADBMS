ff2142b8157de82fa285e7e84cc8c8e4c0475391
Archana <archana@disroot.org>
2018-03-18 22:55:26 +0530

    trying url fix in db_sjet file

--- http/db_sjet.php
+++ http/db_sjet.php
@@ -1,6 +1,6 @@
 <?php 
 
-$url = parse_url(getenv("mysql://b74ef41375c36d:ab7530c1@us-cdbr-iron-east-05.cleardb.net/heroku_69067170d65c976?reconnect=true"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $servername = $url["host"];
 $username = $url["user"];
