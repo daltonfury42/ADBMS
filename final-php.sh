0d3de7f505bbbe886408db0541183f202783e151
Dalton Fury <daltonfury42@disroot.org>
2018-03-19 16:43:42 +0530

    Fixed DB file path

--- final-php.sh
+++ final-php.sh
@@ -3,7 +3,7 @@
 #Parsing DB Cedientials from Heroku //TODO enclose in an if
 IFS=:@/ read uname pass host dbname<<< ${CLEARDB_DATABASE_URL:8:-15}
 #dumping the DB //TODO check if file exists
-mysql --user=$uname --password=$pass --host=$host $dbname < database/db.mysql
+mysql --user=$uname --password=$pass --host=$host $dbname < /root/db.mysql
 
 echo Changing Apache port to $PORT
 
